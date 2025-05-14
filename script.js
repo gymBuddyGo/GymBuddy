b00404e4b117d66a56312e0753c12a4cbafd6e91
Tahlil <bsse0803@iit.du.ac.bd>
2025-05-14 21:49:00 +0600

    Update backend base url

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const password = document.getElementById("register-password").value;
 
             try {
-                const response = await fetch("http://localhost:5000/api/auth/register", {
+                const response = await fetch("https://gymbuddy-su2f.onrender.com/api/auth/register", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ firstname, lastname, email, password })
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const rememberMe = document.getElementById("login-check").checked;
 
             try {
-                const response = await fetch("http://localhost:5000/api/auth/login", {
+                const response = await fetch("https://gymbuddy-su2f.onrender.com/api/auth/login", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ email, password })
